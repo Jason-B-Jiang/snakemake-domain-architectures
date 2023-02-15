80bef3d0a48c9e35abd4e2f07d97aafae5a31307
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2023-02-15 12:42:41 -0500

    tweak setup.sh to initialize empty folders needed to start snakemake pipeline

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# create virtualenv to use snakemake on cluster, and initialize other folders
+# needed to run snakemake workflow
 if [ ! -d "./snakemake_env" ];
 then
     module load python/3.10
@@ -7,6 +11,9 @@ then
 
     pip install --no-index --upgrade pip
     pip install snakemake
+
+    # initialize necessary folders for pipeline
+    mkdir -p resources results
 else
     echo "run 'source snakemake_env/bin/activate' to use Snakemake"
 fi
