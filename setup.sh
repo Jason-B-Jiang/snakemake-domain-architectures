d821bb28f87831cdd929ee1956b874228f96084e
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2023-02-15 11:52:35 -0500

    add check for whether virtualenv for snakemake already created in setup shell script (setup.sh)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
-module load python/3.10
+if [ ! -d "./snakemake_env" ];
+then
+    module load python/3.10
 
-virtualenv --no-download snakemake_env
-source snakemake_env/bin/activate
+    virtualenv --no-download snakemake_env
+    source snakemake_env/bin/activate
 
-pip install --no-index --upgrade pip
-pip install snakemake
+    pip install --no-index --upgrade pip
+    pip install snakemake
+else
+    echo "run 'source snakemake_env/bin/activate' to use Snakemake"
+fi
