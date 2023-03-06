715537f4dd9ed9d28c20b74f4570ef4e93a7ed4f
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2023-03-06 13:12:06 -0500

    fix issues with parallelization of cath-resolve-hits step

--- workflow/scripts/run_cath_resolve_hits.sh
+++ workflow/scripts/run_cath_resolve_hits.sh
@@ -5,7 +5,7 @@
 # Get orthogroup domain architectures with cath-resolve-hits
 #
 # Jason Jiang - Created: Feb/17/2023
-#               Last edited: Feb/17/2023
+#               Last edited: Mar/03/2023
 
 # Command line arguments taken:
 #      $1 = path to hmmscan output for an orthogroup
@@ -15,25 +15,29 @@
 #
 # -----------------------------------------------------------------------------
 
-# check working directory of script at runtime
-# load in command line arguments (input, output)
-# run python helper script to split hmmscan files into resources/tmp folder
-# for each split hmmscan file in resources/tmp/orthogroup, run crh
-# remove resources/tmp folder
-# done
-
 # parse in command line arguments
 hmmscan_hits=$1
 out=$2
+orthogroup=$(basename $hmmscan_hits)
 
 function main {
     # initialize temporary folder for storing split hmmscan hits
     mkdir -p $(pwd)/resources/tmp
 
+    # initialize folder for storing resolved domain architectures from hmmscan
+    # hits by cath-resolve-hits
+    mkdir -p $out
+
     # run python file to create split hmmscan files in temporary folder
     python workflow/scripts/split_hmmscan_file.py ${hmmscan_hits}
 
-    # rm -r "$PWD"/resources/tmp
+    # run cath-resolve-hits on each split hmmscan file
+    for split in $(pwd)/resources/tmp/${orthogroup}/*
+    do
+        cath-resolve-hits --input-format hmmscan_out \
+            --worst-permissible-bitscore 0.1 \
+            ${split} > ${out}/$(basename ${split})
+    done
 }
 
 main
