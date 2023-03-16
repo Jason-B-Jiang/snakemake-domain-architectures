5170c6640ee5fbbe4b637c06042e256f6cef3cab
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2023-03-16 12:10:51 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,19 +2,22 @@
 
 # create virtualenv to use snakemake on cluster, and initialize other folders
 # needed to run snakemake workflow
-if [ ! -d "./snakemake_env" ];
+if [ ! -d "./venv_snakemake" ];
 then
     # uncomment and load in the appropriate python module in your cluster
     # module load python/3.10
 
-    virtualenv snakemake_env
-    source snakemake_env/bin/activate
+    virtualenv venv_snakemake
+    source venv_snakemake/bin/activate
 
     pip install --upgrade pip
     pip install snakemake==7.22.0
 
     # initialize necessary folders for pipeline
     mkdir -p resources results
+
+    # deactivate the virtual environment we just activated
+    deactivate
 else
-    echo "run 'source snakemake_env/bin/activate' to use Snakemake"
+    echo "run 'source venv_snakemake/bin/activate' to use Snakemake"
 fi
